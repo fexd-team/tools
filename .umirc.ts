4350c8f0d85509a8c9307bd9763d14ea4eb958f7
CJY <375564567@qq.com>
2023-11-22 11:23:29 +0800

    docs: fix docs logo

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ import { version } from './package.json'
 export default {
   mode: 'site',
   title: `Fexd Tools`,
-  logo: '/logo.png',
+  logo: process.env.NODE_ENV === 'production' ? '/tools/logo.png' : '/logo.png',
   outputPath: 'docs',
   publicPath: process.env.NODE_ENV === 'production' ? '/tools/' : '/',
   history: { type: 'hash' },
